119c7e42620ae81490ca95740ea2f3471fcfc552
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2024-02-24 18:44:50 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,14 @@
-export default function Header(){
-
+export default function Header({toggleMouseEffect, effectType}){
+    
     return(
+        <>
         <div id="section">
             <h2>Pokemon KingDom</h2>
             <h2>Pokemon KingDom</h2>
         </div>
+        <button id='effect-btn' onClick={toggleMouseEffect}>
+          {effectType == 0 ? "Click me" : 'Change Effect'}  
+        </button>
+        </>
     )
 }
